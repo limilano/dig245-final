4e93ac961e6690413fb802db57455806131ad5a6
limilano <limilano@davidson.edu>
2022-11-14 16:23:04 -0500

    Comment API running in console

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,40 @@
 
 /* javascript */
+
+const sentenceStarters = ['I hate', 'I hate it when', 'I dislike', 'You are wrong because',
+                          'I cannot believe', 'Why should we let you', 'Why do they let people just',
+                          'When will', "Why can't", 'When will they', 'Who cares', "I don't care"];
+
+async function query(data) {
+
+  let comment;
+  let rating;
+
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/CommunityLM/republican-twitter-gpt2",
+        {
+            headers: { Authorization: `Bearer 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` },
+            method: "POST",
+            body: JSON.stringify(data),
+        }
+    );
+
+    comment = await response.json();
+    return comment[0].generated_text;
+}
+
+// query({inputs:sentenceStarters[0]}).then((response) => {
+//     console.log(JSON.stringify(response));
+
+
+    $(document).ready(function () {
+    	$("button").click(async function () {
+    		// await response
+        let random = Math.floor(Math.random() * sentenceStarters.length);
+    		let comment = await query({inputs:sentenceStarters[random]});
+
+    		// because we use "await" ^ we know that data was returned successfully
+    		console.log(comment);
+    	});
+    	$("button").trigger("click");
+});
